e28eeb3022f964311d8f444cdf0bcfbe125ff7e6
pmv <paulomatheusvinhas@gmail.com>
2025-01-25 22:58:35 -0300

    refactor: update JlrsCore UUID and switch to official TokenCompression package

--- promptveil/core/compression/setup.jl
+++ promptveil/core/compression/setup.jl
@@ -3,33 +3,28 @@ using Pkg
 # Ensure we're in the right environment
 Pkg.activate(@__DIR__)
 
-# Develop local TokenCompression package
-token_compression_path = joinpath(@__DIR__, "TokenCompression.jl")
-if isdir(token_compression_path)
-    Pkg.develop(path=token_compression_path)
-else
-    error("TokenCompression.jl not found at $token_compression_path")
-end
-
 # Add registries if needed
 if !isfile(joinpath(DEPOT_PATH[1], "registries", "General", "Registry.toml"))
     Pkg.Registry.add("General")
 end
 
-# Force update to get latest versions
-Pkg.update()
-
-# Ensure CUDA is at version 5
-Pkg.add(name="CUDA", version="5")
+# Add core dependencies first
+Pkg.add([
+    Pkg.PackageSpec(name="JlrsCore", version="0.5.0"),
+    Pkg.PackageSpec(name="TokenCompression", version="0.1.0"),
+    Pkg.PackageSpec(name="CUDA", version="5")
+])
 
 # Add standard dependencies
 Pkg.add([
     "PackageCompiler",
     "SIMD",
     "LinearAlgebra",
-    "Statistics",
-    "JlrsCore"
+    "Statistics"
 ])
+
+# Force update to get latest versions
+Pkg.update()
 
 # Build CUDA if available
 try
